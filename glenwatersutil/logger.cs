eaee76b3ffeb6c587a4a783cbf484e046b05fc15
Glen101noah <119368263+Glen101noah@users.noreply.github.com>
2022-11-29 03:30:33 +0200

    Add files via upload

---
+++ glenwatersutil/logger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace glenwatersutil
+{
+    public static class logger
+    {
+
+        public static void WriteLog(string message) {
+
+            string logPath = ConfigurationManager.AppSettings["logpath"];
+
+            using(StreamWriter writer = new StreamWriter(logPath, true)) {
+
+                writer.WriteLine($"{DateTime.Now} : {message}");
+            
+            
+            
+            }
+        
+        
+        }
+
+    }
+}
